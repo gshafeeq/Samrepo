fbe6e1ccee6c339510817557dcf7ebb602403cb3
gshafeeq <gshafeeq93@gmail.com>
2026-02-23 18:09:51 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -1,5 +1,6 @@
 public class javacode {
     public static void main(String[] args) {
         System.out.println("Welcome to DevOps day1");
+        System.out.println("Welcome to DevOps day2");
     }
 }
